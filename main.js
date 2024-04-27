826f7af788f68f077ba5f2f93aa67ba91d7c98cd
maryamsartaj <168002522+maryamsartaj@users.noreply.github.com>
2024-04-28 00:14:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var members = ["maryam", "ayesha", "sumaira", "sana", "ansar"];
+for (var i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ main.ts
@@ -0,0 +1,4 @@
+let members: string[] =[`maryam`,`ayesha`,`sumaira`,`sana`,`ansar`];
+for(let i=0; i<members.length; i++){
+    console.log(members[i]);
+}
